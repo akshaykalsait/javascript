5574dde85617f8a1a06ee891958c1c139e468091
akshaykalsait <akshaykalsait316@gmail.com>
2023-12-26 20:33:02 +0530

    creating simple information
    creating simple information

---
+++ 01_variableAssign.js
@@ -0,0 +1,37 @@
+
+var myFullName;
+console.log(myFullName);
+myFullName="Akshay Kalsait";
+console.log("Full Name :" , myFullName);
+myFullName="Akshay Rajendra Kalsait";
+console.log("Full Name :" , myFullName);
+
+console.log("*****************************************");
+
+var myLove="I Love Only JS";
+console.log(myLove);
+console.log("*****************************************");
+
+var FirstName="Akshay";
+console.log("First Name :" , FirstName);
+
+var lastName="Kalsait";
+console.log("Last Name :" , lastName);
+
+var age="29";
+console.log("Age :" , age);
+
+var address="Indapur";
+console.log("Address :" ,address);
+
+var pincode="413106";
+console.log("Pincode :" , pincode);
+
+var gender="Male";
+console.log("Gender :" , gender);
+
+var marritialStatus="Yes";
+console.log("Is Married :", marritialStatus);
+
+
+
