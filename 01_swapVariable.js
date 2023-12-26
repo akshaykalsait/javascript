bfd4210c6f441f073acde52e0f16ebb8481ba27b
akshaykalsait <akshaykalsait316@gmail.com>
2023-12-26 20:37:20 +0530

    demo
    demo

---
+++ 01_swapVariable.js
@@ -0,0 +1,24 @@
+
+
+var n1 = "10";
+var n2 = "20";
+var n3 = "30";
+var n4 = "40";
+console.log("Before Swap:");
+console.log("n1 ->", n1);
+console.log("n2 ->", n2);
+console.log("n3 ->", n3);
+console.log("n4 ->", 40);
+
+var temp = n1;
+n1 = n2;
+n2 = temp;
+
+var temp = n3;
+n3 = n4;
+
+console.log("After Swap:");
+console.log("n1 ->", n1);
+console.log("n2 ->", n2);
+console.log("n3 ->", );
+
---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+var num;
+console.log(num);
+var num="10";
+console.log(num); 
+console.log("Akshay"); 
+
+var myName="Akshay K";
+console.log("First Name",myName);
+myName="Akshay Kalsait";
+console.log("Full Name", myName);
