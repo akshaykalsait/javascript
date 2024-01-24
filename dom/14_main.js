b7edd1c7712743f31b0775fd2b5413f9913ebd60
akshaykalsait <akshaykalsait316@gmail.com>
2024-01-25 00:37:45 +0530

    Creating DOM API Assignment
    Creating DOM API Assignment

---
+++ dom/14_main.js
@@ -0,0 +1,19 @@
+
+
+const firstHeading = document.querySelector("#heading");
+firstHeading.style.color = "red";
+
+const secondHeading = document.querySelector("h2");
+secondHeading.style.color = "blue";
+
+const tagP = document.querySelector("p");
+tagP.innerHTML = "Project Description : In this Project I am sharing my Educational Profile."
+
+
+console.log(`--------------------Step 1----------------------`);
+
+console.log(`*** Changing Headind color ***`);
+
+console.log(`--------------------Step 2----------------------`);
+
+console.log(`*** Changing Project Description ***`);
